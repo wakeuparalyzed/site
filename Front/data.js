08411e4e2f32b3975e67d797971c5e516b1a352a
Untils555 <zvyagintseva.1993@list.ru>
2025-05-11 22:08:46 +0300

    Create data.js

---
+++ Front/data.js
@@ -0,0 +1,14 @@
+const allProducts = {
+  notebooks: [
+    { name: "Msi Katana", price: 50000, features: ["Процессор: Intel i5-12450h", "Видеокарта: RTX-3050", "Оперативная память: 16GB", "SSD: 512GB"] },
+    { name: "Asus Rog Strix", price: 70000, features: ["Процессор: Intel i7-12750h", "Видеокарта: RTX-4050", "Оперативная память: 16GB", "SSD: 512GB"] }
+  ],
+  computers: [
+    { name: "Компьютер 1", price: 40000, features: ["AMD Ryzen 5", "8GB RAM", "1TB HDD"] },
+    { name: "Компьютер 2", price: 60000, features: ["Intel i5", "16GB RAM", "512GB SSD"] }
+  ],
+  phones: [
+    { name: "Телефон 1", price: 20000, features: ["Snapdragon 680", "4GB RAM", "64GB"] },
+    { name: "Телефон 2", price: 30000, features: ["Snapdragon 778G", "6GB RAM", "128GB"] }
+  ]
+};
